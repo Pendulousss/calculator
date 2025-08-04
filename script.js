daeafaab88c06bbc4858c906d3c195cb2819b852
Pendulousss <yeshkcm@gmail.com>
2025-08-04 17:57:26 +0000

    Add UI elements

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const operands = document.querySelector("#operands");
 const operators = document.querySelector('#operators')
+const display = document.querySelector('#text')
 
 for (let i=1;i<10;i++){
     let newbtn = document.createElement("button");
@@ -9,7 +10,7 @@ for (let i=1;i<10;i++){
     operands.appendChild(newbtn)
 }
 
-let extrakeys = [0,`.`,`clear`]
+let extrakeys = [0,`.`]
 extrakeys.forEach(element => {
     let newbtn = document.createElement("button");
     newbtn.classList=`numbers`;
@@ -18,11 +19,32 @@ extrakeys.forEach(element => {
     operands.appendChild(newbtn);
 });
 
-let operatorkeys = [`+`,`-`,`*`,`/`];
+let clear = document.createElement("button");
+clear.id = 'clear';
+clear.textContent = `clear`;
+operands.appendChild(clear);
+
+let operatorkeys = [`+`,`-`,`*`,`/`,`=`];
 operatorkeys.forEach(element=>{
     let newbtn = document.createElement('button');
-    newbtn.classList='operands';
+    newbtn.classList='symbols';
     newbtn.textContent=element;
     newbtn.value=element;
     operators.appendChild(newbtn);
 })
+
+//let display = document.querySelector('#display');
+//let eqbtn = document.createElement('button');
+//eqbtn.id = `eqbtn`;
+//eqbtn.textContent = `=`;
+//eqbtn.value = `=`;
+//display.appendChild(eqbtn);
+
+function addition(n,m){return n+m;}
+function subtraction(n,m){return n+m;}
+function multiply(n,m){return n*m;}
+function divide(n,m){return n/m;}
+function exponentiation(n,m){return n**m;}
+
+const numberkeysnl = document.querySelectorAll(".numbers");
+const numberkeys = Array.from(numberkeysnl);
