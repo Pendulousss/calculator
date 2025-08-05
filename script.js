0dc0d29da043f87fa6a7c8db527304d2de9356fe
Pendulousss <yeshkcm@gmail.com>
2025-08-05 17:25:18 +0000

    Add basic calculator logic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const operands = document.querySelector("#operands");
 const operators = document.querySelector('#operators')
 const display = document.querySelector('#text')
+const prevtxt = document.querySelector('#prevtxt')
 
 for (let i=1;i<10;i++){
     let newbtn = document.createElement("button");
@@ -40,11 +41,56 @@ operatorkeys.forEach(element=>{
 //eqbtn.value = `=`;
 //display.appendChild(eqbtn);
 
-function addition(n,m){return n+m;}
-function subtraction(n,m){return n+m;}
+function add(n,m){return +n + +m;}
+function subtract(n,m){return +n - +m;}
 function multiply(n,m){return n*m;}
 function divide(n,m){return n/m;}
-function exponentiation(n,m){return n**m;}
+function exponentiate(n,m){return n**m;}
+
+function operation(n,o,m){
+    if (o ===`+`){result = add(n,m)}
+    else if (o === `-`){result = subtract(n,m)}
+    else if (o === `*`){result = multiply(n,m)}
+    else if (o === `/`){result = divide(n,m)}
+    return result;
+    }
 
 const numberkeysnl = document.querySelectorAll(".numbers");
 const numberkeys = Array.from(numberkeysnl);
+const symbolsnl = document.querySelectorAll('.symbols')
+const symbols = Array.from(symbolsnl);
+
+let currentinput = '';
+let previnput = '';
+let currentop = '';
+numberkeysnl.forEach(key => {
+    key.addEventListener('click',()=>{
+        currentinput += key.value;
+        display.textContent = currentinput;
+    }) 
+});
+symbolsnl.forEach(key=>{
+    key.addEventListener('click',()=>{
+        if (key.value ===`=`){
+           if ( previnput && currentinput && currentop){
+            currentinput = operation(previnput,currentop,currentinput)
+            display.textContent = currentinput
+            previnput = '';
+            currentop = '';
+           }
+        } else if (key.value !== `=`){
+           previnput = currentinput;
+           currentop = key.value;
+           display.textContent = `${currentinput} ${currentop}`;
+           currentinput = '';
+        }
+})
+    })
+
+
+clear.addEventListener('click',()=>{
+    display.textContent = '';
+    currentinput = '';
+    currentop = '';
+    previnput = '';
+})
